63bd7d638bb0d2a46524dad88331e29f568688d9
Ned Zimmerman <ned@bight.ca>
2017-06-21 09:32:55 -0300

    Add documentation.

--- functions.php
+++ functions.php
@@ -3,3 +3,9 @@
  * @author  Pressbooks <code@pressbooks.com>
  * @license GPLv2 (or any later version)
  */
+
+function donham_theme_setup() {
+    add_theme_support( 'pressbooks_global_typography', 'grc', 'he' );
+}
+
+add_action( 'after_setup_theme', 'donham_theme_setup' );
